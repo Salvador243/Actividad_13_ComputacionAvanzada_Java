23da829f89ce4b3c16502ae0f2b8d827dafd6c96
Salvador243 <salvador243gm@gmail.com>
2022-04-30 09:50:57 -0500

    faltan los calculso del imc

--- src/main/java/com/example/demo/controllers/LoginController.java
+++ src/main/java/com/example/demo/controllers/LoginController.java
@@ -12,54 +12,94 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
     @Autowired
     private UsuarioService servicio;
 
-    @RequestMapping("/")
-    public String login(){
+    @RequestMapping(value="/")
+    public String login(HttpServletRequest request,
+                        HttpSession session){
+        if (!session.isNew()) {
+            session.removeAttribute("error");
+        }
         return "index";
     }
-    @RequestMapping("/inicio")
-    public String Inicio() {
+    @RequestMapping("/log")
+    public String Inicio(HttpServletRequest request,
+                         HttpSession session) {
         return "login";
     }
+
     @RequestMapping("/registro")
     public String Registro() {
         return "registro";
     }
 
+    @RequestMapping("/calculo")
+    public String Calculo() {
+        return "Calculo";
+    }
+
     @RequestMapping(value="/guardar", method = {RequestMethod.POST, RequestMethod.TRACE})
     public String guardado(@ModelAttribute("UsuarioModel") UsuarioModel usuario, 
-        BindingResult resultado, ModelMap modelmap){
-        
-        if(resultado.hasErrors())
-            return "registro";
+                           BindingResult resultado,
+                           ModelMap modelmap,
+                           HttpServletRequest request,
+                           HttpSession session){
+        HttpSession sesion = request.getSession();
+        if(resultado.hasErrors()) {
+            session.setAttribute("error", 1);
+            return "redirect:/registro";
+        }
+        ArrayList<UsuarioModel> isExiste = servicio.obtenerPorNick(usuario.getNombreusuario());
+        if(!isExiste.isEmpty()){
+            session.setAttribute("error", 1);
+            return "redirect:/registro";
+        }
 
         servicio.guardarUsuario(usuario);
-        modelmap.addAttribute("nick", usuario.getId());
-
-        return "login";
+        session.setAttribute("idUser", usuario.getId());
+        return "redirect:/calculo";
     }
 
     @RequestMapping(value="/inicio", method = {RequestMethod.POST, RequestMethod.TRACE})
     public String inicio(@ModelAttribute("UsuarioModel") UsuarioModel usuario, 
-        BindingResult resultado, ModelMap modelmap){
+                         BindingResult resultado,
+                         ModelMap modelmap,
+                         HttpServletRequest request,
+                         HttpSession session){
         
-        if(resultado.hasErrors())
-            return "registro";
+        if(resultado.hasErrors()) {
+            modelmap.addAttribute("error", 1);
+            return "redirect:/login";
+        }
+
+        if (!session.isNew()) {
+            session.invalidate();
+        }
+        HttpSession sesion = request.getSession();
+
         String nombreusuario = usuario.getNombreusuario();
         String password = usuario.getPassword();
 
         ArrayList<UsuarioModel> listado = servicio.obtenerPorNick(nombreusuario);
         String pass = "";
-        for (UsuarioModel usuarioModel : listado) pass = usuarioModel.getPassword();
-        if(password.equals(pass))
-            return "Calculo";
 
-        return "registro";
+        for (UsuarioModel usuarioModel : listado) pass = usuarioModel.getPassword();
+        if(password.equals(pass) && !password.equals("")) {
+            modelmap.addAttribute("id", usuario.getId());
+            sesion.setAttribute("id", usuario.getId());
+            return "Calculo";
+        }
+        sesion.setAttribute("error", 1);
+        return "redirect:/log";
     }
 
 }
